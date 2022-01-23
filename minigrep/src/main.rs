1c8a8bcca36291c45d93538877a7f3760773b177
Pedro Palazón Candel <pedro@joyent.com>
2022-01-23 11:02:13 +0100

    minigrep: Updated from 2015 version to 2021 version of the book

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs;
 use std::process;
 
 fn main() {
@@ -19,10 +18,8 @@ fn main() {
 }
 
 fn run(config: Config) {
-    let mut f = File::open(config.filename).expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
