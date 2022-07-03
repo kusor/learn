2838d4c7eaebee6b81c01759e065f732ed075012
Pedro Palazón Candel <pedro@joyent.com>
2022-07-03 11:21:12 +0200

    2 Finished

--- dsasync/src/main.rs
+++ dsasync/src/main.rs
@@ -1,8 +1,11 @@
+use async_std::{fs, prelude::*, stream};
 use async_trait::async_trait;
 use chrono::prelude::*;
 use clap::Parser;
+use futures::future::join_all;
 use log::{info, warn};
 use std::io::{Error, ErrorKind};
+use std::time;
 use yahoo_finance_api as yahoo;
 
 #[derive(Parser, Debug)]
@@ -129,10 +132,10 @@ impl AsyncStockSignal for MinPrice {
 ///
 /// Retrieve data from a data source and extract the closing prices. Errors during download are mapped onto io::Errors as InvalidData.
 ///
-async fn fetch_closing_data(
-    symbol: &str,
-    beginning: &DateTime<Utc>,
-    end: &DateTime<Utc>,
+async fn fetch_closing_data<'a>(
+    symbol: &'a str,
+    beginning: &'a DateTime<Utc>,
+    end: &'a DateTime<Utc>,
 ) -> std::io::Result<Vec<f64>> {
     let provider = yahoo::YahooConnector::new();
 
@@ -151,54 +154,84 @@ async fn fetch_closing_data(
     }
 }
 
-#[async_std::main]
-async fn main() -> std::io::Result<()> {
+///
+/// Do all the calculations for a given symbol once the data is fetch
+///
+async fn get_symbol_values<'a>(
+    symbol: &'a str,
+    from: &'a DateTime<Utc>,
+    to: &'a DateTime<Utc>,
+) -> Option<Vec<f64>> {
+    let closes = fetch_closing_data(&symbol, &from, &to).await.ok()?;
+    if !closes.is_empty() {
+        // min/max of the period. unwrap() because those are Option types
+        let max_price = MaxPrice {};
+        let period_max: f64 = max_price.calculate(&closes).await?;
+        let min_price = MinPrice {};
+        let period_min: f64 = min_price.calculate(&closes).await?;
+        let last_price = *closes.last().unwrap_or(&0.0);
+        let price_diff = PriceDifference {};
+        let (_, pct_change) = price_diff.calculate(&closes).await?;
+        let w_sma = WindowedSMA { window_size: 30 };
+        let sma = w_sma.calculate(&closes).await?;
+
+        // a simple way to output CSV data
+        println!(
+            "{},{},${:.2},{:.2}%,${:.2},${:.2},${:.2}",
+            from.to_rfc3339(),
+            symbol,
+            last_price,
+            pct_change * 100.0,
+            period_min,
+            period_max,
+            sma.last().unwrap_or(&0.0)
+        );
+        info!(
+            "{},{},${:.2},{:.2}%,${:.2},${:.2},${:.2}",
+            from.to_rfc3339(),
+            symbol,
+            last_price,
+            pct_change * 100.0,
+            period_min,
+            period_max,
+            sma.last().unwrap_or(&0.0)
+        )
+    } else {
+        warn!("closes are empty");
+    }
+    Some(closes)
+}
+
+///
+/// Read the list of symbols from a text file, then use future Join to
+/// get the desired values for all the symbols.
+///
+async fn get_values() -> std::io::Result<()> {
     let opts = Opts::parse();
     let from: DateTime<Utc> = opts.from.parse().expect("Couldn't parse 'from' date");
     let to = Utc::now();
+    let fcontents = fs::read_to_string("sp500.txt").await?;
+    let symbols: Vec<&str> = fcontents.split(',').collect();
 
+    let mut fut: Vec<_> = vec![];
     // a simple way to output a CSV header
     println!("period start,symbol,price,change %,min,max,30d avg");
-    for symbol in opts.symbols.split(',') {
-        let closes = fetch_closing_data(symbol, &from, &to).await?;
-        if !closes.is_empty() {
-            // min/max of the period. unwrap() because those are Option types
-            let max_price = MaxPrice {};
-            let period_max: f64 = max_price.calculate(&closes).await.unwrap();
-            let min_price = MinPrice {};
-            let period_min: f64 = min_price.calculate(&closes).await.unwrap();
-            let last_price = *closes.last().unwrap_or(&0.0);
-            let price_diff = PriceDifference {};
-            let (_, pct_change) = price_diff.calculate(&closes).await.unwrap_or((0.0, 0.0));
-            let w_sma = WindowedSMA { window_size: 30 };
-            let sma = w_sma.calculate(&closes).await.unwrap_or_default();
-
-            // a simple way to output CSV data
-            println!(
-                "{},{},${:.2},{:.2}%,${:.2},${:.2},${:.2}",
-                from.to_rfc3339(),
-                symbol,
-                last_price,
-                pct_change * 100.0,
-                period_min,
-                period_max,
-                sma.last().unwrap_or(&0.0)
-            );
-            info!(
-                "{},{},${:.2},{:.2}%,${:.2},${:.2},${:.2}",
-                from.to_rfc3339(),
-                symbol,
-                last_price,
-                pct_change * 100.0,
-                period_min,
-                period_max,
-                sma.last().unwrap_or(&0.0)
-            )
-        } else {
-            warn!("closes are empty");
-        }
+    for symbol in symbols {
+        fut.push(get_symbol_values(&symbol, &from, &to))
     }
+    join_all(fut).await;
     Ok(())
+}
+
+#[async_std::main]
+async fn main() {
+    let duration = time::Duration::from_secs(30);
+    let mut interval = stream::interval(duration);
+
+    // NOTE: The Stream::interval is still unstable
+    while let Some(_) = interval.next().await {
+        get_values().await.unwrap();
+    }
 }
 
 #[cfg(test)]
