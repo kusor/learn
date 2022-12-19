1a40e436d997848748736b15f8e6e3b674022c08
Pedro Palazón Candel <pedro@joyent.com>
2022-12-19 19:16:42 +0100

    Tracing

--- rust-web-dev/src/main.rs
+++ rust-web-dev/src/main.rs
@@ -1,6 +1,8 @@
-use warp::{http::Method, Filter};
+#![warn(clippy::all)]
 
 use handle_errors::return_error;
+use tracing_subscriber::fmt::format::FmtSpan;
+use warp::{http::Method, Filter};
 
 mod routes;
 mod store;
@@ -8,8 +10,19 @@ mod types;
 
 #[tokio::main]
 async fn main() {
+    let log_filter = std::env::var("RUST_LOG")
+        .unwrap_or_else(|_| "practical_rust_book=info,warp=error".to_owned());
+
     let store = store::Store::new();
     let store_filter = warp::any().map(move || store.clone());
+
+    tracing_subscriber::fmt()
+        // Use the filter we built above to determine which traces to record.
+        .with_env_filter(log_filter)
+        // Record an event when each span closes. This can be used to time our
+        // routes' durations!
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
 
     let cors = warp::cors()
         .allow_any_origin()
@@ -21,7 +34,15 @@ async fn main() {
         .and(warp::path::end())
         .and(warp::query())
         .and(store_filter.clone())
-        .and_then(routes::question::get_questions);
+        .and_then(routes::question::get_questions)
+        .with(warp::trace(|info| {
+            tracing::info_span!(
+                "get_questions request",
+                method = %info.method(),
+                path = %info.path(),
+                id = %uuid::Uuid::new_v4(),
+            )
+        }));
 
     let update_question = warp::put()
         .and(warp::path("questions"))
@@ -58,6 +79,7 @@ async fn main() {
         .or(add_answer)
         .or(delete_question)
         .with(cors)
+        .with(warp::trace::request())
         .recover(return_error);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
