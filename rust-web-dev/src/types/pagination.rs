583c7fb5983a7bd4df2d149990fe42b774a67ca0
Pedro Palazón Candel <pedro@joyent.com>
2022-12-19 09:54:53 +0100

    Missing docs and notes

--- rust-web-dev/src/types/pagination.rs
+++ rust-web-dev/src/types/pagination.rs
@@ -2,15 +2,42 @@ use std::collections::HashMap;
 
 use handle_errors::Error;
 
+/// Pagination struct which is getting extract from query params
 #[derive(Debug)]
 pub struct Pagination {
+    /// The index of the first item which has to be returned
     pub start: usize,
+    /// The index of the last item which has to be returned
     pub end: usize,
 }
 
+/// Extract query parameters from the `/questions` route
+///
+/// # Example query
+///
+/// GET requests to this route can have a pagination attached so we just
+/// return the questions we need
+///
+/// `/questions?start=1&end=10`
+///
+/// # Example usage
+///
+/// ```rust
+/// use std::collections::HashMap;
+///
+/// let mut query = HashMap::new();
+/// query.insert("start".to_string(), "1".to_string());
+/// query.insert("end".to_string(), "10".to_string());
+/// let p = pagination::extract_pagination(query).unwrap();
+/// assert_eq!(p.start, 1);
+/// assert_eq!(p.end, 10);
+/// ```
 pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
+    // Needs to check for the existence of just one of the parameters
+    // and return an empty value in case nothing is given for "end" and 1 for "start"
     if params.contains_key("start") && params.contains_key("end") {
         return Ok(Pagination {
+            // Attempt to convert the given "start" & "end" parameters to a number
             start: params
                 .get("start")
                 .unwrap()
