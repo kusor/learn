0dc89aea55e48afe2fc2666fb33b189fb5c3bcb1
Pedro Palazón Candel <pedro@joyent.com>
2022-12-22 12:57:59 +0100

    Update newtype structs to use `serial primary key`(i32) instead of String

--- rust-web-dev/src/types/question.rs
+++ rust-web-dev/src/types/question.rs
@@ -7,5 +7,6 @@ pub struct Question {
     pub content: String,
     pub tags: Option<Vec<String>>,
 }
+
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
-pub struct QuestionId(pub String);
+pub struct QuestionId(pub i32);
