7ca14d276d0a9c026cdcb52b9a7e5fafcf0463ad
Pedro Palazón Candel <pedro@joyent.com>
2022-12-22 13:30:23 +0100

    Updated pagination to use DB offset/limit

--- rust-web-dev/src/types/pagination.rs
+++ rust-web-dev/src/types/pagination.rs
@@ -3,12 +3,12 @@ use std::collections::HashMap;
 use handle_errors::Error;
 
 /// Pagination struct which is getting extract from query params
-#[derive(Debug)]
+#[derive(Default, Debug)]
 pub struct Pagination {
+    /// Optional max number of items to return
+    pub limit: Option<u32>,
     /// The index of the first item which has to be returned
-    pub start: usize,
-    /// The index of the last item which has to be returned
-    pub end: usize,
+    pub offset: u32,
 }
 
 /// Extract query parameters from the `/questions` route
@@ -18,7 +18,7 @@ pub struct Pagination {
 /// GET requests to this route can have a pagination attached so we just
 /// return the questions we need
 ///
-/// `/questions?start=1&end=10`
+/// `/questions?offset=1&limit=10`
 ///
 /// # Example usage
 ///
@@ -26,27 +26,30 @@ pub struct Pagination {
 /// use std::collections::HashMap;
 ///
 /// let mut query = HashMap::new();
-/// query.insert("start".to_string(), "1".to_string());
-/// query.insert("end".to_string(), "10".to_string());
+/// query.insert("limit".to_string(), "1".to_string());
+/// query.insert("offset".to_string(), "10".to_string());
 /// let p = pagination::extract_pagination(query).unwrap();
-/// assert_eq!(p.start, 1);
-/// assert_eq!(p.end, 10);
+/// assert_eq!(p.limit, Some(1));
+/// assert_eq!(p.offset, 10);
 /// ```
 pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
     // Needs to check for the existence of just one of the parameters
     // and return an empty value in case nothing is given for "end" and 1 for "start"
-    if params.contains_key("start") && params.contains_key("end") {
+    if params.contains_key("offset") && params.contains_key("limit") {
         return Ok(Pagination {
-            // Attempt to convert the given "start" & "end" parameters to a number
-            start: params
-                .get("start")
+            // Takes the "limit" parameter in the query and tries to convert it to a number
+            limit: Some(
+                params
+                    .get("limit")
+                    .unwrap()
+                    .parse::<u32>()
+                    .map_err(Error::ParseError)?,
+            ),
+            // Takes the "offset" parameter in the query and tries to convert it to a number
+            offset: params
+                .get("offset")
                 .unwrap()
-                .parse::<usize>()
-                .map_err(Error::ParseError)?,
-            end: params
-                .get("end")
-                .unwrap()
-                .parse::<usize>()
+                .parse::<u32>()
                 .map_err(Error::ParseError)?,
         });
     }
