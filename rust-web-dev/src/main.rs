4c1d5d094bf4bd5ce565f7e66500e5ab451daa75
Pedro Palazón Candel <pedro@joyent.com>
2022-12-27 09:08:08 +0100

    Added authentication

--- rust-web-dev/src/main.rs
+++ rust-web-dev/src/main.rs
@@ -55,6 +55,7 @@ async fn main() {
         .and(warp::path("questions"))
         .and(warp::path::param::<i32>())
         .and(warp::path::end())
+        .and(routes::authentication::auth())
         .and(store_filter.clone())
         .and(warp::body::json())
         .and_then(routes::question::update_question);
@@ -63,12 +64,14 @@ async fn main() {
         .and(warp::path("questions"))
         .and(warp::path::param::<i32>())
         .and(warp::path::end())
+        .and(routes::authentication::auth())
         .and(store_filter.clone())
         .and_then(routes::question::delete_question);
 
     let add_question = warp::post()
         .and(warp::path("questions"))
         .and(warp::path::end())
+        .and(routes::authentication::auth())
         .and(store_filter.clone())
         .and(warp::body::json())
         .and_then(routes::question::add_question);
@@ -76,6 +79,7 @@ async fn main() {
     let add_answer = warp::post()
         .and(warp::path("comments"))
         .and(warp::path::end())
+        .and(routes::authentication::auth())
         .and(store_filter.clone())
         .and(warp::body::form())
         .and_then(routes::answer::add_answer);
--- rust-web-dev/src/routes/authentication.rs
+++ rust-web-dev/src/routes/authentication.rs
@@ -1,10 +1,11 @@
 use argon2::{self, Config};
 use chrono::prelude::*;
 use rand::Rng;
-use warp::http::StatusCode;
+use std::future;
+use warp::http::{Filter, StatusCode};
 
 use crate::store::Store;
-use crate::types::account::{Account, AccountId};
+use crate::types::account::{Account, AccountId, Session};
 
 fn hash_password(password: &[u8]) -> String {
     let salt = rand::thread_rng().gen::<[u8; 32]>();
@@ -47,6 +48,18 @@ pub async fn login(store: Store, login: Account) -> Result<impl warp::Reply, war
     }
 }
 
+pub fn verify_token(token: String) -> Result<Session, handle_errors::Error> {
+    let token = paseto::tokens::validate_local_token(
+        &token,
+        None,
+        "RANDOM WORDS WINTER MACINTOSH PC".as_bytes(),
+        &paseto::tokens::TimeBackend::Chrono,
+    )
+    .map_err(|_| handle_errors::Error::CannotDecryptToken)?;
+
+    serde_json::from_value::<Session>(token).map_err(|_| handle_errors::Error::CannotDecryptToken)
+}
+
 fn verify_password(hash: &str, password: &[u8]) -> Result<bool, argon2::Error> {
     argon2::verify_encoded(hash, password)
 }
@@ -61,4 +74,18 @@ fn issue_token(account_id: AccountId) -> String {
         .set_claim("account_id", serde_json::json!(account_id))
         .build()
         .expect("Failed to construct paseto token w/ builder!")
+}
+
+/// Auth middleware function
+//  Middleware functions return an impl Filter signature with <Extract = (T,), Error =
+//  warp::Rejection> and expect a ` std::future::Ready` as return value
+pub fn auth() -> impl Filter<Extract = (Session,), Error = warp::Rejection> + Clone {
+    warp::header::<String>("Authorization").and_then(|token: String| {
+        let token = match verify_token(token) {
+            Ok(t) => t,
+            Err(_) => return future::ready(Err(warp::reject::reject())),
+        };
+
+        future::ready(Ok(token))
+    })
 }
