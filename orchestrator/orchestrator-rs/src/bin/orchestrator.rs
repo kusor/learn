cc6b5339ce0867322629f5822dc38d07661f1719
Pedro Palazón Candel <pedro@joyent.com>
2023-01-05 09:04:13 +0100

    Orchestrator Chapter #3 in Rust

--- orchestrator/orchestrator-rs/src/bin/orchestrator.rs
+++ orchestrator/orchestrator-rs/src/bin/orchestrator.rs
@@ -1,4 +1,6 @@
+use bollard::errors::Error;
 use chrono::prelude::*;
+use tokio::time::{sleep, Duration};
 use uuid::Uuid;
 
 #[path = "../node.rs"]
@@ -6,28 +8,61 @@ mod node;
 #[path = "../task.rs"]
 mod task;
 
-fn main() {
-    println!("Hello orchestrator!");
+#[allow(dead_code)]
+async fn create_container(
+) -> Result<task::DockerClient<String>, Box<dyn std::error::Error + 'static>> {
+    let c = task::Config::new("test-container-1".to_string(), "alpine:3".to_string(), None);
+    let mut dc = task::DockerClient::new(c)?;
+    let dr = dc.run().await?;
+    dc.container_id = dr.container_id;
+    Ok(dc)
+}
+
+async fn stop_container(
+    dc: &task::DockerClient<String>,
+) -> Result<task::DockerResult<String>, Box<dyn std::error::Error + 'static>> {
+    if let Some(container_id) = &dc.container_id {
+        let dr = dc.stop(container_id).await?;
+        Ok(dr)
+    } else {
+        Ok(task::DockerResult {
+            error: Some(Error::DockerResponseServerError {
+                status_code: 422,
+                message: "Missing container id".to_string(),
+            }),
+            action: "stop".to_string(),
+            container_id: None,
+            result: Some("failed".to_string()),
+        })
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    log::info!("Hello orchestrator!");
 
     let t = task::Task {
         id: Uuid::new_v4(),
-        name: "Task-1".to_string(),
-        state: task::State::Pending(0),
-        image: "Image-1".to_string(),
-        memory: 1024,
-        disk: 1,
-        restart_policy: "always".to_string(),
+        name: Some("Task-1".to_string()),
+        state: task::State::Pending,
+        image: Some("Image-1".to_string()),
+        memory: Some(1024),
+        disk: Some(1),
+        restart_policy: Some("always".to_string()),
+        ..Default::default()
     };
+
+    log::info!("task: {:#?}\n", &t);
 
     let te = task::TaskEvent {
         id: Uuid::new_v4(),
-        state: task::State::Pending(0),
+        state: task::State::Pending,
         timestamp: Utc::now(),
-        task: &t,
+        task: t,
     };
 
-    println!("task: {:#?}\n", t);
-    println!("task event: {:#?}\n", te);
+    log::info!("task event: {:#?}\n", te);
 
     let n = node::Node {
         name: "Node-1".to_string(),
@@ -41,5 +76,28 @@ fn main() {
         role: "worker".to_string(),
     };
 
-    println!("node: {:#?}\n", n)
+    log::info!("node: {:#?}\n", n);
+
+    match create_container().await {
+        Err(error) => {
+            log::error!("Failed to create the container: {:#?}\n", error);
+        }
+        Ok(dc) => {
+            sleep(Duration::from_secs(5)).await;
+            log::info!("created container with id: {:#?}\n", &dc.container_id);
+
+            match stop_container(&dc).await {
+                Err(error) => {
+                    log::error!("Failed to stop the container: {:#?}\n", error);
+                }
+                Ok(dr) => {
+                    if dr.error.is_some() {
+                        log::error!("Failed to stop the container: {:#?}\n", dr.error);
+                    } else {
+                        log::info!("Container successfully removed");
+                    }
+                }
+            };
+        }
+    };
 }
--- orchestrator/orchestrator-rs/src/task.rs
+++ orchestrator/orchestrator-rs/src/task.rs
@@ -1,34 +1,196 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
+use bollard::{
+    container::Config as ContainerConfig, container::RemoveContainerOptions as RemoveOptions,
+    container::StopContainerOptions as StopOptions, errors::Error, image::CreateImageOptions,
+    Docker,
+};
+use futures_util::TryStreamExt;
+
 use chrono::prelude::*;
 use uuid::Uuid;
 
-pub type StateVal = u32;
-
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq, Default)]
 pub enum State {
-    Pending(StateVal),
-    //     Scheduled(StateVal),
-    //     Completed(StateVal),
-    //     Running(StateVal),
-    //     Failed(StateVal),
+    #[default]
+    Pending,
+    //     Scheduled,
+    //     Completed,
+    //     Running,
+    //     Failed,
 }
 
-#[derive(Debug, Clone)]
-pub struct Task {
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
+pub struct Task<T>
+where
+    T: Into<String> + Eq + Hash,
+{
     pub id: Uuid,
-    pub name: String,
+    pub name: Option<T>,
     pub state: State,
-    pub image: String,
-    pub memory: u64,
-    pub disk: u64,
-    // ExposedPorts  nat.PortSet
-    // PortBindings  map[string]string
-    pub restart_policy: String,
+    pub image: Option<T>,
+    pub memory: Option<u64>,
+    pub disk: Option<u64>,
+    // Not absolutely sure of the format of these, we'll see
+    pub exposed_ports: Option<HashMap<T, HashMap<(), ()>>>,
+    pub port_bindings: Option<HashMap<T, T>>,
+    pub restart_policy: Option<T>,
+    pub start_time: Option<DateTime<Utc>>,
+    pub finish_time: Option<DateTime<Utc>>,
 }
 
-#[derive(Debug, Clone)]
-pub struct TaskEvent<'a> {
+#[derive(Debug, Clone, Default, PartialEq)]
+pub struct TaskEvent<T>
+where
+    T: Into<String> + Eq + Hash,
+{
     pub id: Uuid,
     pub state: State,
     pub timestamp: DateTime<Utc>,
-    pub task: &'a Task,
+    pub task: Task<T>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq)]
+pub struct Config<T>
+where
+    T: Into<String> + Eq + Hash,
+{
+    pub name: Option<T>,
+    pub attach_stdin: Option<bool>,
+    pub attach_stdout: Option<bool>,
+    pub attach_stderr: Option<bool>,
+    pub exposed_ports: Option<HashMap<T, HashMap<(), ()>>>,
+    pub cmd: Option<Vec<T>>,
+    pub image: Option<T>,
+    pub cpu: Option<f64>,
+    pub memory: Option<u64>,
+    pub disk: Option<u64>,
+    pub env: Option<Vec<T>>,
+    pub restart_policy: Option<T>,
+}
+
+#[derive(Debug, Clone)]
+pub struct DockerClient<T>
+where
+    T: Into<String> + Eq + Hash,
+{
+    pub client: Docker,
+    pub config: Config<T>,
+    pub container_id: Option<T>,
+}
+#[allow(dead_code)]
+pub struct DockerResult<T> {
+    pub error: Option<Error>,
+    pub action: T,
+    pub container_id: Option<T>,
+    pub result: Option<T>,
+}
+
+impl DockerResult<String> {
+    pub fn new(
+        error: Option<Error>,
+        action: String,
+        container_id: Option<String>,
+        result: Option<String>,
+    ) -> Self {
+        Self {
+            error,
+            action,
+            container_id,
+            result,
+        }
+    }
+}
+
+impl Config<String> {
+    pub fn new(name: String, image: String, env: Option<Vec<String>>) -> Self {
+        Self {
+            name: Some(name),
+            image: Some(image),
+            env,
+            ..Default::default()
+        }
+    }
+}
+
+impl DockerClient<String> {
+    #[allow(dead_code)]
+    pub fn new(config: Config<String>) -> Result<Self, Box<dyn std::error::Error + 'static>> {
+        let docker = Docker::connect_with_socket_defaults()?;
+        Ok(Self {
+            client: docker,
+            config,
+            container_id: None,
+        })
+    }
+
+    #[allow(dead_code)]
+    pub async fn run(&self) -> Result<DockerResult<String>, Box<dyn std::error::Error + 'static>> {
+        let image: String = match &self.config.image {
+            Some(img) => img.to_string(),
+            None => "alpine:3".to_string(),
+        };
+
+        self.client
+            .create_image::<&str>(
+                Some(CreateImageOptions {
+                    from_image: image.as_str(),
+                    ..Default::default()
+                }),
+                None,
+                None,
+            )
+            .try_collect::<Vec<_>>()
+            .await?;
+
+        let container_id = self
+            .client
+            .create_container::<&str, &str>(
+                None,
+                ContainerConfig {
+                    image: Some(&image),
+                    tty: Some(true),
+                    ..Default::default()
+                },
+            )
+            .await?
+            .id;
+
+        self.client
+            .start_container::<String>(&container_id, None)
+            .await?;
+
+        Ok(DockerResult::new(
+            None,
+            "start".to_string(),
+            Some(container_id),
+            Some("success".to_string()),
+        ))
+    }
+
+    #[allow(dead_code)]
+    pub async fn stop(
+        &self,
+        container_id: &str,
+    ) -> Result<DockerResult<String>, Box<dyn std::error::Error + 'static>> {
+        self.client
+            .stop_container(container_id, Some(StopOptions { t: 15 }))
+            .await?;
+        self.client
+            .remove_container(
+                container_id,
+                Some(RemoveOptions {
+                    force: true,
+                    ..Default::default()
+                }),
+            )
+            .await?;
+        Ok(DockerResult::new(
+            None,
+            "stop".to_string(),
+            Some(container_id.to_string()),
+            Some("success".to_string()),
+        ))
+    }
 }
